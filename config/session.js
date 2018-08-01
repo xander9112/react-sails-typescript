bbad948e67504c0ef99991c5548bb00b286d5223
AlexZander <xander91@mail.ru>
2018-08-02 00:40:21 +0300

    Fix build

--- config/session.js
+++ config/session.js
@@ -11,29 +11,31 @@
 
 module.exports.session = {
 
-  /***************************************************************************
-  *                                                                          *
-  * Session secret is automatically generated when your new app is created   *
-  * Replace at your own risk in production-- you will invalidate the cookies *
-  * of your users, forcing them to log in again.                             *
-  *                                                                          *
-  ***************************************************************************/
-  secret: '24e5cb956b7a8e88d38709b8eb0796ef',
+    /***************************************************************************
+     *                                                                          *
+     * Session secret is automatically generated when your new app is created   *
+     * Replace at your own risk in production-- you will invalidate the cookies *
+     * of your users, forcing them to log in again.                             *
+     *                                                                          *
+     ***************************************************************************/
+    secret: '24e5cb956b7a8e88d38709b8eb0796ef',
 
-
-  /***************************************************************************
-  *                                                                          *
-  * Customize when built-in session support will be skipped.                 *
-  *                                                                          *
-  * (Useful for performance tuning; particularly to avoid wasting cycles on  *
-  * session management when responding to simple requests for static assets, *
-  * like images or stylesheets.)                                             *
-  *                                                                          *
-  * https://sailsjs.com/config/session                                       *
-  *                                                                          *
-  ***************************************************************************/
-  // isSessionDisabled: function (req){
-  //   return !!req.path.match(req._sails.LOOKS_LIKE_ASSET_RX);
-  // },
+    cookie: {
+        secure: true,
+    },
+    /***************************************************************************
+     *                                                                          *
+     * Customize when built-in session support will be skipped.                 *
+     *                                                                          *
+     * (Useful for performance tuning; particularly to avoid wasting cycles on  *
+     * session management when responding to simple requests for static assets, *
+     * like images or stylesheets.)                                             *
+     *                                                                          *
+     * https://sailsjs.com/config/session                                       *
+     *                                                                          *
+     ***************************************************************************/
+    // isSessionDisabled: function (req){
+    //   return !!req.path.match(req._sails.LOOKS_LIKE_ASSET_RX);
+    // },
 
 };
